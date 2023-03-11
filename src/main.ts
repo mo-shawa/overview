4a8e089febcc90806f32d0f565d34e7e96758a4e
moshawa <moshawa0@gmail.com>
2023-03-11 16:58:05 -0500

    basic earth texture

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import "./style.css"
 import * as THREE from "three"
 
+const loader = new THREE.TextureLoader()
+const earthMap = loader.load(
+	"1_earth_8k.jpg",
+	() => console.log("success"),
+	() => console.log("progress"),
+	(error) => console.error(error)
+)
+
 const scene = new THREE.Scene()
 
 const sizes = {
@@ -9,6 +17,7 @@ const sizes = {
 }
 
 const renderer = new THREE.WebGLRenderer()
+
 renderer.setSize(sizes.width, sizes.height)
 document.body.append(renderer.domElement)
 
@@ -34,19 +43,27 @@ camera.position.z = 5
 scene.add(camera)
 
 const testmesh = new THREE.Mesh(
-	new THREE.BoxGeometry(1, 1, 1),
-	new THREE.MeshMatcapMaterial()
+	new THREE.SphereGeometry(2, 400, 400),
+	new THREE.MeshStandardMaterial({
+		map: earthMap,
+	})
 )
 
-console.log(testmesh)
+testmesh.castShadow = true
+testmesh.receiveShadow = true
 
 scene.add(testmesh)
+
+const sun = new THREE.PointLight("white", 1, 100)
+sun.position.set(10, 0, 10)
+
+scene.add(sun)
 
 renderer.render(scene, camera)
 
 function tick() {
 	requestAnimationFrame(tick)
-	testmesh.rotation.y += 0.01
+	testmesh.rotation.y += 0.00005
 	renderer.render(scene, camera)
 }
 
