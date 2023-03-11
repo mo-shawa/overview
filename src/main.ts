b1dcb33510415ec437fdcaf14a317d097df964d6
moshawa <moshawa0@gmail.com>
2023-03-11 16:06:54 -0500

    bare scene

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css"
+import * as THREE from "three"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene()
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const renderer = new THREE.WebGLRenderer()
+document.body.append(renderer.domElement)
+
+const sizes = {
+	height: window.innerHeight,
+	width: window.innerWidth,
+}
+
+const camera = new THREE.PerspectiveCamera(
+	65,
+	sizes.width / sizes.height,
+	0.01,
+	1000000
+)
+
+renderer.render(scene, camera)
