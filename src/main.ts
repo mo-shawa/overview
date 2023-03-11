8d0b97dc673cc1505d8c148307e30ca1b5875586
moshawa <moshawa0@gmail.com>
2023-03-11 16:21:51 -0500

    fix resize + pixelation

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import * as THREE from "three"
 
 const scene = new THREE.Scene()
 
-const renderer = new THREE.WebGLRenderer()
-document.body.append(renderer.domElement)
-
 const sizes = {
 	height: window.innerHeight,
 	width: window.innerWidth,
 }
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(sizes.width, sizes.height)
+document.body.append(renderer.domElement)
 
 const camera = new THREE.PerspectiveCamera(
 	65,
@@ -18,4 +19,35 @@ const camera = new THREE.PerspectiveCamera(
 	1000000
 )
 
+window.addEventListener("resize", () => {
+	sizes.height = window.innerHeight
+	sizes.width = window.innerWidth
+
+	renderer.setSize(sizes.width, sizes.height)
+	camera.aspect = sizes.width / sizes.height
+
+	camera.updateProjectionMatrix()
+})
+
+camera.position.z = 5
+
+scene.add(camera)
+
+const testmesh = new THREE.Mesh(
+	new THREE.BoxGeometry(1, 1, 1),
+	new THREE.MeshMatcapMaterial()
+)
+
+console.log(testmesh)
+
+scene.add(testmesh)
+
 renderer.render(scene, camera)
+
+function tick() {
+	requestAnimationFrame(tick)
+	testmesh.rotation.y += 0.01
+	renderer.render(scene, camera)
+}
+
+tick()
